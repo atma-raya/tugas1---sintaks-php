7f01a2d814b24b5784602181d9404ca8ed58b3d4
nizarul <faizinnizarul@gmail.com>
2023-02-20 19:12:56 +0700

    tugas 1

---
+++ tugas1/index.php
@@ -0,0 +1,28 @@
+<?php
+$data = "Topi";
+$data2 = "Bundar";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1.0">
+    <title>tugas 1a</title>
+</head>
+<body>
+   
+
+    <h2>
+       <i> 
+        "<?php echo $data ?> saya <?php echo $data2 ?>, <?php echo $data2 ?> <?php echo $data ?> saya."
+       </i> 
+    </h2>
+
+
+
+</body>
+</html>
